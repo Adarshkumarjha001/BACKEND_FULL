7c3988094db07bdf97b916e64f45449145b59662
Adarshkumarjha001 <adarshkumarjha1308@gmail.com>
2026-01-04 23:29:54 +0530

    Updated backend files

---
+++ .history/BACKEND/IMPEXP/second_20260104232942.js
@@ -0,0 +1,14 @@
+console.log("This is the second file for Import and Export backend");
+
+function sum(a,b){
+    return a + b;
+
+}
+// console.log(sum(12,5));
+
+function sub(a,b){
+    return a - b;
+}
+// console.log(sub(10,5));
+
+module.exports = {sum,su
