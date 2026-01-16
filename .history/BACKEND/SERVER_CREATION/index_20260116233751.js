413f139131ccadab77a726d234876b9026865862
Adarshkumarjha001 <adarshkumarjha1308@gmail.com>
2026-01-16 23:38:30 +0530

    updated things

---
+++ .history/BACKEND/SERVER_CREATION/index_20260116233751.js
@@ -0,0 +1,21 @@
+require("http");
+
+const http=require("http");
+
+const server=http.createServer((req,res)=>{
+    if(req.url==="/"){
+        res.end("Welcome to the Home Page of Server Creation Module");
+    }
+    else if(req.url==="/about"){
+        res.end("This is the About Page of Server Creation Module");
+    }
+    else if(req.url==="/contact"){
+        res.end("This is the Contact Page of Server Creation Module");
+    }
+    res.end("Hello World! This is the Server Creation Module";
+})
+
+server.listen(3000,()=>{
+    console.log("Server is listening on port 3000");
+}); 
+
