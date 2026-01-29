dcb150dcbc1f018794cd86653f1090ca656ae8e9
Adarshkumarjha001 <adarshkumarjha1308@gmail.com>
2026-01-29 23:08:51 +0530

    updated files

--- .history/BACKEND/SERVER_CREATION/index_20260129230842.js
+++ .history/BACKEND/SERVER_CREATION/index_20260129230842.js
@@ -16,7 +16,7 @@ const server=http.createServer((req,res)=>{
 })
 
 server.listen(3000,()=>{
-    console.log("Server is listening on port 3000");
+    console.log("Server is listening on port 3000"
 }); 
 
 
