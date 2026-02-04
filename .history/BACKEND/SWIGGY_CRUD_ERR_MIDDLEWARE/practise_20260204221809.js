e01650a5ce74ea530153f4381c5662fdb02f4aa6
Adarshkumarjha001 <adarshkumarjha1308@gmail.com>
2026-02-04 22:18:22 +0530

    New Files for practicising

--- .history/BACKEND/SWIGGY_CRUD_ERR_MIDDLEWARE/practise_20260204221809.js
+++ .history/BACKEND/SWIGGY_CRUD_ERR_MIDDLEWARE/practise_20260204221809.js
@@ -6,7 +6,6 @@ app.use(express.json());
 
 app.post("/data",(req,res)=>{
     const data=req.body;
-    res.status(201).send({message:"Data received successfully",data:data});
-}
+    res.status(201).send({
 
 
--- .history/BACKEND/SWIGGY_CRUD_ERR_MIDDLEWARE/practise_20260204221810.js
+++ .history/BACKEND/SWIGGY_CRUD_ERR_MIDDLEWARE/practise_20260204221810.js
@@ -7,6 +7,5 @@ app.use(express.json());
 app.post("/data",(req,res)=>{
     const data=req.body;
     res.status(201).send({message:"Data received successfully",data:data});
-}
 
 
