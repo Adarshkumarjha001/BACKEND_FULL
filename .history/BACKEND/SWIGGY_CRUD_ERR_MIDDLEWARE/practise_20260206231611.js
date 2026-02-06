8b137b3f5e00f5f5939991e28cf12800c1bc7010
Adarshkumarjha001 <adarshkumarjha1308@gmail.com>
2026-02-06 23:16:20 +0530

    New Files for practicising

---
+++ .history/BACKEND/SWIGGY_CRUD_ERR_MIDDLEWARE/practise_20260206231611.js
@@ -0,0 +1,15 @@
+const express=require("express");
+
+const app=express();
+
+app.use(express.json());    
+
+app.post("/data",(req,res)=>{
+    const data=req.body;
+    res.status(201).send({message:"Data received successfully",data:data};
+}
+
+
+
+
+
